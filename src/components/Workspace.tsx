6139a0cf2814dfc01b6ac39e5600489e6f5869ba
Manoel Prado <manoel.prado.neto@accenture.com>
2024-01-03 20:05:12 -0300

    input validation

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -1,12 +1,33 @@
 import { PlusCircle } from '@phosphor-icons/react';
 
 import styles from './Workspace.module.css';
+import { ChangeEvent, InvalidEvent, useState } from 'react';
 
 export function Workspace() {
+  const [newTaskText, setNewTaskText] = useState('');
+
+  console.log(newTaskText)
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('');
+    setNewTaskText(event.target.value)
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>){
+    event.target.setCustomValidity("Esse campo é obrigatório!")
+  }
+
   return (
     <div className={styles.wrapper}>
       <form className={styles.newTask}>
-        <input type="text" placeholder='Adicione uma nova tarefa' />
+        <input 
+          type="text" 
+          onChange={handleNewTaskChange}
+          value={newTaskText}
+          onInvalid={handleNewTaskInvalid}
+          placeholder='Adicione uma nova tarefa'
+          required
+        />
 
         <button type='submit'>
           Criar <PlusCircle fontSize={16}/>
