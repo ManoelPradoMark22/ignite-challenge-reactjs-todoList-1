69152e873d0fbc39c76eab2acbfb0919b3176dbc
Manoel Prado <manoel.prado.neto@accenture.com>
2024-01-03 19:17:33 -0300

    create and style the NewTak Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { PlusCircle } from '@phosphor-icons/react';
+
 import { Header } from './components/Header';
+
 import './global.css';
+
+import styles from './App.module.css';
 
 export function App() {
 
   return (
-    <Header/>
+    <div>
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <form className={styles.newTask}>
+          <input type="text" placeholder='Adicione uma nova tarefa' />
+
+          <button type='submit'>
+            Criar <PlusCircle fontSize={16}/>
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
